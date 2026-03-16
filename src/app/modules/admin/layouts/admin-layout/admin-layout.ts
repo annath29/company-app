d2d2b8355db1d7b86fb0c07e03e3cdb9fcbfe441
Andrea <guerreroaanathalia@gmail.com>
2026-03-16 11:29:08 -0500

    side-menu importado desde el paquete de npm

--- src/app/modules/admin/layouts/admin-layout/admin-layout.ts
+++ src/app/modules/admin/layouts/admin-layout/admin-layout.ts
@@ -1,14 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SideMenu } from '../../../shared/components/side-menu/side-menu';
+import { AngSideMenu, TitleColor } from 'ang-side-menu'
 
 @Component({
   selector: 'app-admin-layout',
-  imports: [RouterOutlet, SideMenu],
+  imports: [RouterOutlet, AngSideMenu],
   templateUrl: './admin-layout.html',
 })
 export default class AdminLayout {
   isAuthenticated = signal(false);
+
+  TitleColor = TitleColor;
+
   onLogin() {
     this.isAuthenticated.set(true);
   }
